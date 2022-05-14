eae4f9015e05d0cca46e0e86741722df1653b294
Danilo Silva <danilo.silva@msn.com>
2022-05-14 02:01:31 -0300

    Aula 03 - Comentários

---
+++ CSharp - Arquitetura com DDD/CSharpBasico/Aula03Comentarios/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Aula03Comentarios
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+
+            // Comentário de uma linha
+
+            /*
+            Comentário
+            de
+            Múltiplas
+            Linhas
+            */
+        }
+    }
+}
