1bbc98b97c21f741d671fbc51bcd62ffaa044dc6
Danilo Silva <danilo.silva@msn.com>
2022-05-14 12:44:30 -0300

    Aula 12 - Switch / Case

---
+++ CSharp - Arquitetura com DDD/CSharpBasico/Aula12SwitchCase/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Aula12SwitchCase
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Switch... case (Caso)
+
+            int x = 1;
+
+            switch (x)
+            {
+                case 1:
+                    Console.WriteLine("Igual a 1");
+                    break;
+                case 2:
+                    Console.WriteLine("Igual a 2");
+                    break;
+                case 3:
+                    Console.WriteLine("Igual a 3");
+                    break;
+                default:
+                    Console.WriteLine("Default");
+                    break;
+            }
+        }
+    }
+}
