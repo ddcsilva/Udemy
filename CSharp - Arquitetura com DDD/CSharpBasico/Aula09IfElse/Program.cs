b1e472326a11880654a8c059cb71d397f85c10df
Danilo Silva <danilo.silva@msn.com>
2022-05-14 12:36:48 -0300

    Aula 09 - If/Else

---
+++ CSharp - Arquitetura com DDD/CSharpBasico/Aula09IfElse/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Aula09IfElse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // IF - SE
+
+            var valor1 = 10;
+            var valor2 = 20;
+
+            if (valor1 == valor2)
+            {
+                Console.WriteLine("igual");
+            }
+            else
+            {
+                Console.WriteLine("Diferente");
+            }
+        }
+    }
+}
