cc22dc24d0bf23558107cc27cd7b1e60e4327cb4
Danilo Silva <danilo.silva@msn.com>
2022-05-14 12:35:39 -0300

    Aula 08 - Operadores Relacionais

---
+++ CSharp - Arquitetura com DDD/CSharpBasico/Aula08OperadoresRelacionais/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Aula08OperadoresRelacionais
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // == Igual a
+            // != Diferente de
+            //  > Maior que
+            //  < Menor que
+            // >= Maior ou igual a
+            // <= Menor ou igual a
+
+            var valor1 = 10;
+            var valor2 = 20;
+
+            Console.WriteLine(valor1 == valor2);
+            Console.WriteLine(valor1 != valor2);
+            Console.WriteLine(valor1 > valor2);
+            Console.WriteLine(valor1 <  valor2);
+            Console.WriteLine(valor1 >= valor2);
+            Console.WriteLine(valor1 <= valor2);
+        }
+    }
+}
