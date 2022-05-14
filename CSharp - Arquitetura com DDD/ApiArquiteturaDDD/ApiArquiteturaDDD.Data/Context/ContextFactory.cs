45c8e60f2913599c928904d0e0019f1aa756eb7d
Danilo Silva <danilo.silva@msn.com>
2022-05-14 14:42:17 -0300

    Aula 56 - Criação da classe ContextFactory

---
+++ CSharp - Arquitetura com DDD/ApiArquiteturaDDD/ApiArquiteturaDDD.Data/Context/ContextFactory.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace ApiArquiteturaDDD.Data.Context
+{
+    public class ContextFactory : IDesignTimeDbContextFactory<MyContext>
+    {
+        public MyContext CreateDbContext(string[] args)
+        {
+            // Usado para criar as migrações
+            var connectionString = "Server=localhost;Port=3306;Database=CourseApiDDD;Uid=root;Pwd=root";
+            var optionsBuilder = new DbContextOptionsBuilder<MyContext>();
+            optionsBuilder.UseMySql(connectionString);
+            return new MyContext(optionsBuilder.Options);
+        }
+    }
+}
