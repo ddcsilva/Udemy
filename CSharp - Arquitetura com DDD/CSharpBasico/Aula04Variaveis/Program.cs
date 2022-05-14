5ad27037f879e3c6dcba775b6bad5b136c88e0dc
Danilo Silva <danilo.silva@msn.com>
2022-05-14 02:06:52 -0300

    Aula 04 - Declaração de Variáveis

---
+++ CSharp - Arquitetura com DDD/CSharpBasico/Aula04Variaveis/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Aula04Variaveis
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int variavelTeste;
+            variavelTeste = 123;
+
+            Console.WriteLine("O valor da variável é: {0}", variavelTeste);
+
+            bool variavelBooleana = false;
+
+            if (variavelBooleana == false)
+            {
+                variavelTeste = 765;
+            }
+
+            Console.WriteLine("O valor da variável é: {0}", variavelTeste);
+
+            var testeInteger = 111;
+            var testeBool = true;
+            var testeString = "Aula";
+
+            Console.WriteLine(testeInteger);
+            Console.WriteLine(testeBool);
+            Console.WriteLine(testeString);
+        }
+    }
+}
