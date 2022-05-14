342321b07ff3fbb22ee772f28d5850d184e86bba
Danilo Silva <danilo.silva@msn.com>
2022-05-14 12:38:53 -0300

    Aula 10 - For

---
+++ CSharp - Arquitetura com DDD/CSharpBasico/Aula10For/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Aula10For
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // FOR - Para cada
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(i.ToString());
+            }
+        }
+    }
+}
