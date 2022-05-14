dd1594308cc8ff1821e644ea6d646108b97104fc
Danilo Silva <danilo.silva@msn.com>
2022-05-14 12:41:56 -0300

    Aula 11 - While / Continue / Break

---
+++ CSharp - Arquitetura com DDD/CSharpBasico/Aula11While/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Aula11While
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 0;
+
+            while (x < 10)
+            {
+                x++;
+
+                if (x % 2 == 0)
+                {
+                    continue;
+                }
+
+                if (x == 7)
+                {
+                    break;
+                }
+
+                Console.WriteLine(x);
+            }
+        }
+    }
+}
