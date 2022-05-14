8ae7f22a3756ab29f113a9ab682fe3c26e030f27
Danilo Silva <danilo.silva@msn.com>
2022-05-14 10:58:06 -0300

    Aula 05 - Operadores Aritméticos

---
+++ CSharp - Arquitetura com DDD/CSharpBasico/Aula05OperadoresAritmeticos/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Aula05OperadoresAritmeticos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // + Adição
+            // - Subtração
+            // * Multiplicação
+            // / Divisão
+            // % Resto da Divisão
+
+            int valor1, valor2, total;
+            
+            valor1 = 30;
+            valor2 = 55;
+
+            total = valor1 + valor2;
+            Console.WriteLine("Total é de {0}", total);
+        }
+    }
+}
