59df24a6b5e6adc5c93f1ca9cc7477431cf9f124
Danilo Silva <danilo.silva@msn.com>
2022-05-14 11:00:20 -0300

    Aula 06 - Incremento e Decremento

---
+++ CSharp - Arquitetura com DDD/CSharpBasico/Aula06IncrementoDecremento/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Aula06IncrementoDecremento
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int valor1, valor2, total;
+
+            valor1 = 50;
+            valor2 = 40;
+
+            total = valor1 + valor2;
+
+            total++;
+            total--;
+
+            Console.WriteLine("Total é de {0}", total);
+        }
+    }
+}
