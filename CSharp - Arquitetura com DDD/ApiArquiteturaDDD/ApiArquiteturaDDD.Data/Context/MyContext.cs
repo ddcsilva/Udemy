403c42dd09d44e90968669ace10dbfe92aef38d0
Danilo Silva <danilo.silva@msn.com>
2022-05-14 14:11:14 -0300

    Aula 55 - Criação do MyContext e Referenciar Api.Data com Api.Domain

---
+++ CSharp - Arquitetura com DDD/ApiArquiteturaDDD/ApiArquiteturaDDD.Data/Context/MyContext.cs
@@ -0,0 +1,17 @@
+﻿using ApiArquiteturaDDD.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiArquiteturaDDD.Data.Context
+{
+    public class MyContext : DbContext
+    {
+        public MyContext(DbContextOptions<MyContext> options) : base(options) { }
+
+        public DbSet<UserEntity> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
