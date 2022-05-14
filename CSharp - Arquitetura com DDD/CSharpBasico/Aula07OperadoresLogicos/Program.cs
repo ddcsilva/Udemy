36632b0c9d8b8351c13280902f45e408d31d1161
Danilo Silva <danilo.silva@msn.com>
2022-05-14 11:03:26 -0300

    Aula 07 - Operadores Lógicos

---
+++ CSharp - Arquitetura com DDD/CSharpBasico/Aula07OperadoresLogicos/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Aula07OperadoresLogicos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // AND = E   = &&
+            //  OR = OU  = ||
+            // NOT = NÃO = !
+
+            bool condicao1 = true;
+            bool condicao2 = false;
+
+            Console.WriteLine(condicao1 && condicao2);
+            Console.WriteLine(condicao1 || condicao2);
+            Console.WriteLine(!condicao1);
+            Console.WriteLine(!condicao2);
+        }
+    }
+}
