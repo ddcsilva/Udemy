9f712ff7555bbfe563126f223fa6c2feb617a186
Danilo Silva <danilo.silva@msn.com>
2022-05-14 23:08:56 -0300

    Aula 59 - Criação da Interface de Repository

---
+++ CSharp - Arquitetura com DDD/ApiArquiteturaDDD/ApiArquiteturaDDD.Domain/Interfaces/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using ApiArquiteturaDDD.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ApiArquiteturaDDD.Domain.Interfaces
+{
+    public interface IRepository<T> where T : BaseEntity
+    {
+        Task<T> InsertAsync(T entity);
+        Task<T> UpdateAsync(T entity);
+        Task<bool> DeleteAsync(Guid id);
+        Task<T> SelectAsync(Guid id);
+        Task<IEnumerable<T>> ListAsync();
+    }
+}
