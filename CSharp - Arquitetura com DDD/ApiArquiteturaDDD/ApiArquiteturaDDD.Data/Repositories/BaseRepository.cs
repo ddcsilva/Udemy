dcd555f330723c88227ac890f69bb422bd2ab916
Danilo Silva <danilo.silva@msn.com>
2022-05-14 23:38:10 -0300

    Aula 62 - DeleteAsync do Repositório Genérico

--- CSharp - Arquitetura com DDD/ApiArquiteturaDDD/ApiArquiteturaDDD.Data/Repositories/BaseRepository.cs
+++ CSharp - Arquitetura com DDD/ApiArquiteturaDDD/ApiArquiteturaDDD.Data/Repositories/BaseRepository.cs
@@ -19,9 +19,26 @@ namespace ApiArquiteturaDDD.Data.Repositories
             _dataset = _context.Set<T>();
         }
 
-        public Task<bool> DeleteAsync(Guid id)
+        public async Task<bool> DeleteAsync(Guid id)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var result = await _dataset.SingleOrDefaultAsync(t => t.Id.Equals(id));
+
+                if (result == null)
+                {
+                    return false;
+                }
+
+                _dataset.Remove(result);
+                await _context.SaveChangesAsync();
+                
+                return true;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
         }
 
         public async Task<T> InsertAsync(T entity)
