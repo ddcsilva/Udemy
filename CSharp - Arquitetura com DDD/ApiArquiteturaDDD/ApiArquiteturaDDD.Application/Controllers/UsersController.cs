dba9aec79d22b2c3b848ab77c543fcf9ee5e39f8
Danilo Silva <danilo.silva@msn.com>
2022-05-15 01:32:58 -0300

    Aula 69 - Criação do Método GetAll [HttpGet]

--- CSharp - Arquitetura com DDD/ApiArquiteturaDDD/ApiArquiteturaDDD.Application/Controllers/UsersController.cs
+++ CSharp - Arquitetura com DDD/ApiArquiteturaDDD/ApiArquiteturaDDD.Application/Controllers/UsersController.cs
@@ -1,6 +1,31 @@
-﻿namespace ApiArquiteturaDDD.Application.Controllers
+﻿using ApiArquiteturaDDD.Domain.Interfaces.Services.User;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace ApiArquiteturaDDD.Application.Controllers
 {
-    public class UsersController
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
     {
+        [HttpGet]
+        public async Task<ActionResult> GetAll([FromServices] IUserService service)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+                return Ok(await service.GetAll());
+            }
+            catch (ArgumentException ex)
+            {
+                return StatusCode((int)HttpStatusCode.InternalServerError, ex.Message);
+            }
+        }
     }
 }
