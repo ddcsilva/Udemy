433c49d1003ff3675cc0ad8015d98678d253a2fe
Danilo Silva <danilo.silva@msn.com>
2022-05-14 23:30:08 -0300

    Aula 60 - InsertAsync do Repositório Genérico

---
+++ CSharp - Arquitetura com DDD/ApiArquiteturaDDD/ApiArquiteturaDDD.Data/Repositories/BaseRepository.cs
@@ -0,0 +1,64 @@
+﻿using ApiArquiteturaDDD.Data.Context;
+using ApiArquiteturaDDD.Domain.Entities;
+using ApiArquiteturaDDD.Domain.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ApiArquiteturaDDD.Data.Repositories
+{
+    public class BaseRepository<T> : IRepository<T> where T : BaseEntity
+    {
+        protected readonly MyContext _context;
+        private DbSet<T> _dataset;
+
+        public BaseRepository(MyContext context)
+        {
+            _context = context;
+            _dataset = _context.Set<T>();
+        }
+
+        public Task<bool> DeleteAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<T> InsertAsync(T entity)
+        {
+            try
+            {
+                if (entity.Id == Guid.Empty)
+                {
+                    entity.Id = Guid.NewGuid();
+                }
+
+                entity.CreateAt = DateTime.UtcNow;
+                _dataset.Add(entity);
+
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+            return entity;
+        }
+
+        public Task<IEnumerable<T>> ListAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> SelectAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> UpdateAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
