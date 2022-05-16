bb039685d97af909eeba4440d664deca53bd716b
Danilo Silva <danilo.silva@msn.com>
2022-05-15 21:05:26 -0300

    Aula 79 - Implementação da UserImplementation

---
+++ CSharp - Arquitetura com DDD/ApiArquiteturaDDD/ApiArquiteturaDDD.Data/Implementations/UserImplementation.cs
@@ -0,0 +1,24 @@
+﻿using ApiArquiteturaDDD.Data.Context;
+using ApiArquiteturaDDD.Data.Repositories;
+using ApiArquiteturaDDD.Domain.Entities;
+using ApiArquiteturaDDD.Domain.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace ApiArquiteturaDDD.Data.Implementations
+{
+    internal class UserImplementation : BaseRepository<UserEntity>, IUserRepository
+    {
+        private DbSet<UserEntity> _dataset;
+
+        public UserImplementation(MyContext context) : base(context)
+        {
+            _dataset = _context.Set<UserEntity>();
+        }
+
+        public async Task<UserEntity> FindByLogin(string email)
+        {
+            return await _dataset.FirstOrDefaultAsync(u => u.Email.Equals(email));
+        }
+    }
+}
