b5ad81d7c04221c7c5dbeb0979c7781d70e3c2ed
Danilo Silva <danilo.silva@msn.com>
2022-05-15 21:33:18 -0300

    Aula 82 - Implementação da LoginController

---
+++ CSharp - Arquitetura com DDD/ApiArquiteturaDDD/ApiArquiteturaDDD.Application/Controllers/LoginController.cs
@@ -0,0 +1,46 @@
+﻿using ApiArquiteturaDDD.Domain.Entities;
+using ApiArquiteturaDDD.Domain.Interfaces.Services.User;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace ApiArquiteturaDDD.Application.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<object> Login([FromBody] UserEntity userEntity, [FromServices] ILoginService service)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            if (userEntity == null)
+            {
+                return BadRequest();
+            }
+
+            try
+            {
+                var result = await service.FindByLogin(userEntity);
+
+                if (result != null)
+                {
+                    return result;
+                }
+                else
+                {
+                    return NotFound();
+                }
+            }
+            catch (ArgumentException ex)
+            {
+                return StatusCode((int)HttpStatusCode.InternalServerError, ex.Message);
+            }
+        }
+
+    }
+}
