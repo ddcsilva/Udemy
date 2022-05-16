2011b237161f16893fd420dcdea0f591020a7c0b
Danilo Silva <danilo.silva@msn.com>
2022-05-15 21:07:26 -0300

    Aula 80 - Criação da ILoginService

---
+++ CSharp - Arquitetura com DDD/ApiArquiteturaDDD/ApiArquiteturaDDD.Domain/Interfaces/Services/User/ILoginService.cs
@@ -0,0 +1,10 @@
+﻿using ApiArquiteturaDDD.Domain.Entities;
+using System.Threading.Tasks;
+
+namespace ApiArquiteturaDDD.Domain.Interfaces.Services.User
+{
+    public interface ILoginService
+    {
+        Task<object> FindByLogin(UserEntity user);
+    }
+}
