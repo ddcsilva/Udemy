d8654e9252cff6c93e256cf78c3d57d8b675a954
Danilo Silva <danilo.silva@msn.com>
2022-05-15 21:00:49 -0300

    Aula 78 - Implementação de IUserRepository

---
+++ CSharp - Arquitetura com DDD/ApiArquiteturaDDD/ApiArquiteturaDDD.Domain/Interfaces/IUserRepository.cs
@@ -0,0 +1,10 @@
+﻿using ApiArquiteturaDDD.Domain.Entities;
+using System.Threading.Tasks;
+
+namespace ApiArquiteturaDDD.Domain.Interfaces
+{
+    public interface IUserRepository : IRepository<UserEntity>
+    {
+        Task<UserEntity> FindByLogin(string email);
+    }
+}
