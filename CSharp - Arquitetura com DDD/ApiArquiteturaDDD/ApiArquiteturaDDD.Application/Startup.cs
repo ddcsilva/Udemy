973b52b11c2f2c6ee805421f21b456bb6f9757d4
Danilo Silva <danilo.silva@msn.com>
2022-05-16 00:52:52 -0300

    Aula 91 - Adição do Botão Authorize no Swagger

--- CSharp - Arquitetura com DDD/ApiArquiteturaDDD/ApiArquiteturaDDD.Application/Startup.cs
+++ CSharp - Arquitetura com DDD/ApiArquiteturaDDD/ApiArquiteturaDDD.Application/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Options;
 using Microsoft.OpenApi.Models;
 using System;
+using System.Collections.Generic;
 
 namespace ApiArquiteturaDDD.Application
 {
@@ -85,6 +86,25 @@ namespace ApiArquiteturaDDD.Application
                     {
                         Name = "Termo de Licença de Uso",
                         Url = new Uri("http://www.mfrinfo.com.br")
+                    }
+                });
+
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description = "Entre com o Token JWT",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey
+                });
+
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement {
+                    {
+                        new OpenApiSecurityScheme {
+                            Reference = new OpenApiReference {
+                                Id = "Bearer",
+                                Type = ReferenceType.SecurityScheme
+                            }
+                        }, new List<string>()
                     }
                 });
             });
