2e764c4620679407bde1ee88c0c3562fe291e3e2
Danilo Silva <danilo.silva@msn.com>
2022-05-21 02:04:26 -0300

    Aula 11 - Registros de usuários - Parte 3

--- CSharp - Gerenciador de Condomínios/GerenciadorCondominios/GerenciadorCondominios.DAL/Interfaces/IUsuarioRepositorio.cs
+++ CSharp - Gerenciador de Condomínios/GerenciadorCondominios/GerenciadorCondominios.DAL/Interfaces/IUsuarioRepositorio.cs
@@ -1,8 +1,15 @@
 ﻿using GerenciadorCondominios.BLL.Models;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
 
 namespace GerenciadorCondominios.DAL.Interfaces
 {
     public interface IUsuarioRepositorio : IRepositorioGenerico<Usuario>
     {
+        int VerificarSeExisteRegistro();
+
+        Task EfetuarLogin(Usuario usuario, bool lembrar);
+        Task<IdentityResult> CriarUsuario(Usuario usuario, string senha);
+        Task IncluirUsuarioEmFuncao(Usuario usuario, string funcao);
     }
 }
