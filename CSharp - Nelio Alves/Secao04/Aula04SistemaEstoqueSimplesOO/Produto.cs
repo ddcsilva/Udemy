7f7c3d7aa24f005f535ccd7902337ff5e7ad2b39
Danilo Silva <danilo.silva@msn.com>
2022-05-31 22:56:36 -0300

    Aula 42 - Começando a resolver um segundo problema exemplo

---
+++ CSharp - Nelio Alves/Secao04/Aula04SistemaEstoqueSimplesOO/Produto.cs
@@ -0,0 +1,14 @@
+﻿namespace Aula04SistemaEstoqueSimplesOO
+{
+    public class Produto
+    {
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+        public double ValorTotalEmEstoque()
+        {
+            return Preco * Quantidade;
+        }
+    }
+}
