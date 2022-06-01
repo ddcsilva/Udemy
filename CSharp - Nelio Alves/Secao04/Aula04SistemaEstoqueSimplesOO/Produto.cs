5acda0a6bc5e97c980f16cd0a7fc2e1baece13e8
Danilo Silva <danilo.silva@msn.com>
2022-05-31 23:39:39 -0300

    Aula 44 - Terminando de construir o programa

--- CSharp - Nelio Alves/Secao04/Aula04SistemaEstoqueSimplesOO/Produto.cs
+++ CSharp - Nelio Alves/Secao04/Aula04SistemaEstoqueSimplesOO/Produto.cs
@@ -13,6 +13,15 @@ namespace Aula04SistemaEstoqueSimplesOO
             return Preco * Quantidade;
         }
 
+        public void AdicionarProdutos(int quantidade)
+        {
+            Quantidade += quantidade;
+        }
+        public void RemoverProdutos(int quantidade)
+        {
+            Quantidade -= quantidade;
+        }
+
         public override string ToString()
         {
             return Nome + ", $ " + Preco.ToString("F2", CultureInfo.InvariantCulture) + ", " + Quantidade + " unidades, Total: $ " + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
--- CSharp - Nelio Alves/Secao04/Aula04SistemaEstoqueSimplesOO/Program.cs
+++ CSharp - Nelio Alves/Secao04/Aula04SistemaEstoqueSimplesOO/Program.cs
@@ -17,7 +17,24 @@ namespace Aula04SistemaEstoqueSimplesOO
             Console.Write("Quantidade no Estoque: ");
             produto.Quantidade = int.Parse(Console.ReadLine());
 
+            Console.WriteLine();
             Console.WriteLine("Dados do produto: " + produto);
+
+            Console.WriteLine();
+            Console.Write("Digite o número de produtos a ser adicionado ao estoque: ");
+            int quantidade = int.Parse(Console.ReadLine());
+            produto.AdicionarProdutos(quantidade);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados atualizados: " + produto);
+
+            Console.WriteLine();
+            Console.Write("Digite o número de produtos a ser removido do estoque: ");
+            quantidade = int.Parse(Console.ReadLine());
+            produto.RemoverProdutos(quantidade);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados atualizados: " + produto);
         }
     }
 }
