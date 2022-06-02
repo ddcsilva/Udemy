5ad5115b865692cc69b3891454ec7e99d6b47e96
Danilo Silva <danilo.silva@msn.com>
2022-06-02 20:38:16 -0300

    Exercício 06 (Sessão 04)

---
+++ CSharp - Nelio Alves/Secao04/Exercicio06/ConversorDeMoeda.cs
@@ -0,0 +1,15 @@
+﻿namespace Exercicio06
+{
+    public class ConversorDeMoeda
+    {
+        static public double Cotacao;
+
+        static public double MoedaDolar;
+
+        static public double Conversor()
+        {
+            double total = Cotacao * MoedaDolar;
+            return (total + (total * 0.06));
+        }
+    }
+}
