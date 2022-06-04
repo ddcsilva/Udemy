7028d83b85e50b1a97242f83fc8147d1fc5edb9b
Danilo Silva <danilo.silva@msn.com>
2022-06-04 11:23:24 -0300

    Aula 68 - Coalescência Nula

---
+++ CSharp - Nelio Alves/Secao06/Aula02CoalescenciaNula/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Aula02CoalescenciaNula
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double? x = null;
+            double? y = 10;
+
+            double number1 = x ?? 5;
+            double number2 = y ?? 5;
+
+            Console.WriteLine(number1);
+            Console.WriteLine(number2);
+        }
+    }
+}
