9f8182ea09a91fd0a4aa35cd00f54c2d9895d269
Danilo Silva <danilo.silva@msn.com>
2022-06-04 03:30:10 -0300

    Exercício 01 (Seção 05)

---
+++ CSharp - Nelio Alves/Secao05/Exercicio01/ContaCorrente.cs
@@ -0,0 +1,57 @@
+﻿using System.Globalization;
+
+namespace Exercicio01
+{
+    internal class ContaCorrente
+    {
+        private string _nomeTitular;
+
+        public int NumeroConta { get; private set; }
+        public double Saldo { get; private set; }
+
+        public ContaCorrente(int numeroConta, string nomeTitulo)
+        {
+            NumeroConta = numeroConta;
+            _nomeTitular = nomeTitulo;
+            Saldo = 0;
+        }
+
+        public ContaCorrente(int numeroConta, string nomeTitulo, double saldoInicial) : this(numeroConta, nomeTitulo)
+        {
+            Saldo = saldoInicial;
+        }
+
+        public string NomeTitular
+        {
+            get { return _nomeTitular; }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    _nomeTitular = value;
+                }
+            }
+        }
+
+        public void Depositar(double valor)
+        {
+            if (valor > 0)
+            {
+                Saldo += valor;
+            }
+        }
+
+        public void Sacar(double valor)
+        {
+            if (valor > 0)
+            {
+                Saldo -= (valor + 5.00); 
+            }
+        }
+
+        public override string ToString()
+        {
+            return "Conta " + NumeroConta + ", Titular: " + NomeTitular + ", Saldo: " + Saldo.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
