8d1e23dde605c9c53fd702b9e1a79cd15f91c512
Danilo Silva <danilo.silva@msn.com>
2022-06-05 12:24:05 -0300

    Aula 73 - Modificadores de parâmetros ref e out

---
+++ CSharp - Nelio Alves/Secao06/Aula06ParametrosRefOut/Calculadora.cs
@@ -0,0 +1,15 @@
+﻿namespace Aula06ParametrosRefOut
+{
+    public class Calculadora
+    {
+        public static void Triplicar(ref int x)
+        {
+            x = x * 3;
+        }
+
+        public static void Duplicar(int origem, out int resultado)
+        {
+            resultado = origem * 2;
+        }
+    }
+}
---
+++ CSharp - Nelio Alves/Secao06/Aula06ParametrosRefOut/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Aula06ParametrosRefOut
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            Calculadora.Triplicar(ref a);
+            Console.WriteLine(a);
+
+            int b = 10;
+            int duplicado;
+            Calculadora.Duplicar(b, out duplicado);
+            Console.WriteLine(duplicado);
+        }
+    }
+}
