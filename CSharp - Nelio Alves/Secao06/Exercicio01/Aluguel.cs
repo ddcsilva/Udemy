4a5c90ce4783c58f829aad81b8a725e77044264c
Danilo Silva <danilo.silva@msn.com>
2022-06-05 01:12:03 -0300

    Exercício 01 (Seção 06)

---
+++ CSharp - Nelio Alves/Secao06/Exercicio01/Aluguel.cs
@@ -0,0 +1,19 @@
+﻿namespace Exercicio01
+{
+    public class AluguelQuarto
+    {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+
+        public AluguelQuarto(string nome, string email)
+        {
+            Nome = nome;
+            Email = email;
+        }
+
+        public override string ToString()
+        {
+            return Nome + ", " + Email;
+        }
+    }
+}
---
+++ CSharp - Nelio Alves/Secao06/Exercicio01/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Exercicio01
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            AluguelQuarto[] quarto = new AluguelQuarto[9];
+            Console.Write("Quantos quartos você gostaria de alugar? ");
+            int quantidadeQuartos = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+
+            for (int i = 0; i < quantidadeQuartos; i++)
+            {
+                Console.Write("Qual o nome do estudante? ");
+                string Nome = Console.ReadLine();
+                Console.Write("Qual o email do estudante? ");
+                string Email = Console.ReadLine();
+                Console.Write("Qual quarto [0-9] deseja reservar? ");
+                int NumeroQuarto = int.Parse(Console.ReadLine());
+
+                quarto[NumeroQuarto] = new AluguelQuarto(Nome, Email);
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("Quartos ocupados:");
+
+            for (int i = 0; i < quarto.Length; i++)
+            {
+                if (quarto[i] != null)
+                {
+                    Console.WriteLine(i + ": " + quarto[i]);
+                }
+            }
+        }
+    }
+}
