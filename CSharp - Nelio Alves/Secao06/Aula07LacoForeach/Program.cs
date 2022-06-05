5a2e37a9a12ad75381fda076ef00a083574bf5cf
Danilo Silva <danilo.silva@msn.com>
2022-06-05 12:28:05 -0300

    Aula 75 - Sintaxe Foreach

---
+++ CSharp - Nelio Alves/Secao06/Aula07LacoForeach/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Aula07LacoForeach
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] vetor = new string[] { "Maria", "Alex", "Bob" };
+
+            foreach (string item in vetor)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
