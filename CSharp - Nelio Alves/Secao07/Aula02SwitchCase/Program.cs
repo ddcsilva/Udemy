da3f0ad6a2e8b04c3ac714e78ce5347cd747a991
Danilo Silva <danilo.silva@msn.com>
2022-06-05 14:23:54 -0300

    Aula 86 - Sintaxe Switch-Case

---
+++ CSharp - Nelio Alves/Secao07/Aula02SwitchCase/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Aula02SwitchCase
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+            string dia;
+
+            switch (x)
+            {
+                case 1:
+                    dia = "Domingo";
+                    break;
+                case 2:
+                    dia = "Segunda-feira";
+                    break;
+                case 3:
+                    dia = "Terça-feira";
+                    break;
+                case 4:
+                    dia = "Quarta-feira";
+                    break;
+                case 5:
+                    dia = "Quinta-feira";
+                    break;
+                case 6:
+                    dia = "Sexta-feira";
+                    break;
+                case 7:
+                    dia = "Sábado";
+                    break;
+                default:
+                    dia = "Valor Inválido";
+                    break;
+            }
+
+            Console.WriteLine("Dia: " + dia);
+        }
+    }
+}
