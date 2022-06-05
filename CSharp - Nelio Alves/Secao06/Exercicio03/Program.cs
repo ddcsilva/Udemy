d4859f4642f17ef7ac0fcf74c20983fefb48bcd1
Danilo Silva <danilo.silva@msn.com>
2022-06-05 14:11:40 -0300

    Exercício 03 (Seção 06)

---
+++ CSharp - Nelio Alves/Secao06/Exercicio03/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Exercicio03
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Insira o tamanho da Matriz N por N: ");
+            int n = int.Parse(Console.ReadLine());
+
+            int[,] matriz = new int[n, n];
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] valores = Console.ReadLine().Split(' ');
+
+                for (int j = 0; j < n; j++)
+                {
+                    matriz[i, j] = int.Parse(valores[j]);
+                }
+            }
+
+            Console.WriteLine("Diagonal Principal: ");
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(matriz[i, i] + " ");
+            }
+
+            Console.WriteLine();
+
+            int contador = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (matriz[i, j] < 0)
+                    {
+                        contador++;
+                    }
+                }
+            }
+
+            Console.WriteLine("Números negativos: " + contador);
+        }
+    }
+}
