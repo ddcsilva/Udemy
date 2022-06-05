517e91fe062dae8087aa8b92b623f764205b6883
Danilo Silva <danilo.silva@msn.com>
2022-06-05 12:32:56 -0300

    Aula 76 - Listas (Parte 1)

---
+++ CSharp - Nelio Alves/Secao06/Aula08UsandoList/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Aula08UsandoList
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> lista = new List<string>();
+
+            List<string> lista2 = new List<string>() { "Maria", "Alex" };
+        }
+    }
+}
