1dd2b214c0e39b897c21084938e2cb4d112f3150
Danilo Silva <danilo.silva@msn.com>
2022-06-06 00:30:58 -0300

    Aula 87 - Expressão Condicional Ternária

---
+++ CSharp - Nelio Alves/Secao07/Aula03CondicaoTernaria/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Aula03CondicaoTernaria
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            double preco = 34.5;
+            double desconto;
+
+            if (preco < 20.0)
+            {
+                desconto = preco * 0.1;
+            }
+            else
+            {
+                desconto = preco * 0.05;
+            }
+
+            double preco2 = 34.5;
+            double desconto2 = (preco2 < 20.0) ? preco2 * 0.1 : preco2 * 0.05;
+
+            Console.WriteLine(desconto);
+            Console.WriteLine(desconto2);
+        }
+    }
+}
