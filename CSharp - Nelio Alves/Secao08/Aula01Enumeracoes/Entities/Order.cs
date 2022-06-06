0b05a61880f21bde03fa6ca08da39ba5635dc0fe
Danilo Silva <danilo.silva@msn.com>
2022-06-06 16:38:14 -0300

    Aula 124 - Enumerações

---
+++ CSharp - Nelio Alves/Secao08/Aula01Enumeracoes/Entities/Order.cs
@@ -0,0 +1,17 @@
+﻿using Aula01Enumeracoes.Entities.Enums;
+using System;
+
+namespace Aula01Enumeracoes.Entities
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public DateTime Moment { get; set; }
+        public OrderStatus Status { get; set; }
+
+        public override string ToString()
+        {
+            return Id + ", " + Moment + ", " + Status;
+        }
+    }
+}
---
+++ CSharp - Nelio Alves/Secao08/Aula01Enumeracoes/Program.cs
@@ -0,0 +1,27 @@
+﻿using Aula01Enumeracoes.Entities;
+using Aula01Enumeracoes.Entities.Enums;
+using System;
+
+namespace Aula01Enumeracoes
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Order order = new Order
+            {
+                Id = 1080,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PendingPayment
+            };
+
+            Console.WriteLine(order);
+
+            string statusString = OrderStatus.PendingPayment.ToString();
+            OrderStatus statusString2 = Enum.Parse<OrderStatus>("Delivered");
+
+            Console.WriteLine(statusString);
+            Console.WriteLine(statusString2);
+        }
+    }
+}
