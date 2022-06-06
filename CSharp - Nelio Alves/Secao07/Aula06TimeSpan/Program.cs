4e7697c71f9f6fece5772f11073f2ea72efbf4af
Danilo Silva <danilo.silva@msn.com>
2022-06-06 00:40:20 -0300

    Aula 90 - TimeSpan (Representando Durações)

---
+++ CSharp - Nelio Alves/Secao07/Aula06TimeSpan/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Aula06TimeSpan
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            TimeSpan t1;
+            TimeSpan t2;
+            TimeSpan t3;
+            TimeSpan t4;
+            TimeSpan t5;
+            TimeSpan t6;
+
+            // Construtores
+
+            t1 = new TimeSpan();
+            t2 = new TimeSpan(900000000L);
+            t3 = new TimeSpan(2, 11, 21);
+            t4 = new TimeSpan(1, 2, 11, 21);
+            t5 = new TimeSpan(1, 2, 11, 21, 321);
+
+            Console.WriteLine(t1);
+            Console.WriteLine(t2);
+            Console.WriteLine(t3);
+            Console.WriteLine(t4);
+            Console.WriteLine(t5);
+
+            // Métodos From
+
+            t1 = TimeSpan.FromDays(1.5);
+            t2 = TimeSpan.FromHours(1.5);
+            t3 = TimeSpan.FromMinutes(1.5);
+            t4 = TimeSpan.FromSeconds(1.5);
+            t5 = TimeSpan.FromMilliseconds(1.5);
+            t6 = TimeSpan.FromTicks(900000000L);
+
+            Console.WriteLine(t1);
+            Console.WriteLine(t2);
+            Console.WriteLine(t3);
+            Console.WriteLine(t4);
+            Console.WriteLine(t5);
+            Console.WriteLine(t6);
+        }
+    }
+}
