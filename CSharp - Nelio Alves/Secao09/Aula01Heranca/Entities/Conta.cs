ecd03c1a393bf1240db41a8724c9557cc80bd6a7
Danilo Silva <danilo.silva@msn.com>
2022-06-07 19:13:59 -0300

    Aula 135 - Herança

---
+++ CSharp - Nelio Alves/Secao09/Aula01Heranca/Entities/Conta.cs
@@ -0,0 +1,31 @@
+﻿namespace Aula01Heranca.Entities
+{
+    public class Conta
+    {
+        public int Numero { get; protected set; }
+        public string Titular { get; protected set; }
+        public double Saldo { get; protected set; }
+
+        public Conta()
+        {
+
+        }
+
+        public Conta(int numero, string titular, double saldo)
+        {
+            Numero = numero;
+            Titular = titular;
+            Saldo = saldo;
+        }
+
+        public void Sacar(double quantia)
+        {
+            Saldo -= quantia;
+        }
+
+        public void Depositar(double quantia)
+        {
+            Saldo += quantia;
+        }
+    }
+}
---
+++ CSharp - Nelio Alves/Secao09/Aula01Heranca/Program.cs
@@ -0,0 +1,15 @@
+﻿using Aula01Heranca.Entities;
+using System;
+
+namespace Aula01Heranca
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaComercial conta = new ContaComercial(8010, "Bob Brown", 100.0, 500.0);
+
+            Console.WriteLine(conta.Saldo);
+        }
+    }
+}
